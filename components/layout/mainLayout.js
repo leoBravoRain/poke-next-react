6c3678a69eb72868a82c5d0703a556df0e0eb8b6
leo <leo.bravo.rain@gmail.com>
2021-10-27 16:28:22 -0300

    add naive pagination

--- components/layout/mainLayout.js
+++ components/layout/mainLayout.js
@@ -1,10 +1,27 @@
+import Head from 'next/head';
+
 const MainLayout = ({children}) => {
   return (
-    <>
-      <div>Nav bar</div>
-      <main>{children}</main>
-      <div>footer</div>
-    </>
+      <>
+          <Head>
+
+              {/* tailwind material resources */}
+              <link
+                  href="https://fonts.googleapis.com/icon?family=Material+Icons"
+                  rel="stylesheet"
+              />
+              {/* font awesome */}
+              <link
+                  rel="stylesheet"
+                  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+                  integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
+                  crossOrigin="anonymous"
+              />
+          </Head>
+          <div>Nav bar</div>
+          <main>{children}</main>
+          <div>footer</div>
+      </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@
 // import Image from "next/image";
 // import Button from "@material-tailwind/react/Button";
 import PokeCard from "../components/general/Card";
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import axios from "axios";
+
+import Icon from "@material-tailwind/react/Icon";
 
 const pokemons = [
   {
@@ -60,38 +62,36 @@ const pokemons = [
 
 // api limits
 const limit = 10;
-const offset = 0;
+// const offset = 0;
 
 export default function Home() {
-  // stats
+  // statates
   const [pokemons, setPokemons] = useState([]);
+  const [offset, setOffset] = useState(0);
 
-  // get data
-  const getPokemons = async () => {
+  // get pokemon data
+  const getPokemons = async (offsetToUse) => {
+    // console.log("call new pokemons");
+    // console.log("current offset", offset);
+
     const resp = await axios
-      .get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+      .get(
+        `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offsetToUse}`
+      )
       .catch((err) => console.log("Error:", err));
 
-    // console.log(resp);
-    // var pokemons = resp.data.results;
+    const pokemonsToDisplay = resp.data.results;
 
-    // // upper case first character
-    // pokemons = pokemons.map((pok) => {
-    //   pok["name"] = pok.name[0].toUpperCase() + pok.name.slice(1);
-    // });
-
-    // set pokemons
-    // setPokemons(resp.data.results);
+    // console.log(pokemonsToDisplay);
 
     // get pokemon data
-    getPokemonData(resp.data.results);
-  };
+    // getPokemonData(resp.data.results);
 
-  const getPokemonData = async (pokemons) => {
+    // console.log("get detailed data from pokemons");
     const pokemonsList = [];
 
     await Promise.all(
-      pokemons.map((pokemon) => {
+      pokemonsToDisplay.map((pokemon) => {
         return axios
           .get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
           .then((result) => {
@@ -101,29 +101,73 @@ export default function Home() {
     );
 
     pokemonsList = pokemonsList.map((pok) => {
-        var pokemon = {
-          name: pok.name[0].toUpperCase() + pok.name.slice(1),
-          photo: pok.sprites.front_default,
-          id: pok.id,
-        };
+      return {
+        name: pok.name[0].toUpperCase() + pok.name.slice(1),
+        photo: pok.sprites.front_default,
+        id: pok.id,
+      };
 
-        return pokemon;
+      // return pokemon;
     });
 
-    console.log(pokemonsList);
+    // sort by id
+    pokemonsList = pokemonsList.sort((a, b) => a.id - b.id);
+
+    // console.log("pokemons to display: ", pokemonsList);
 
     // set pokemons
     setPokemons(pokemonsList);
-
   };
 
+  // callback function of offset change state
   useEffect(() => {
     // get data
-    getPokemons();
-  }, []);
+    getPokemons(offset);
+  }, [offset]);
+
+  // change page
+  const changePageHandler = (arrow) => {
+    // console.log("change page: ", arrow);
+    // previous
+    if (arrow === "left") {
+      if (offset > 0) {
+        // update offset
+        setOffset((prev) => prev - limit);
+      }
+    }
+
+    // next
+    else {
+      // here it should check the max page
+      // update offset
+      setOffset(offset + limit);
+    }
+  };
 
   return (
     <div className="p-2">
+      {/* pagination */}
+      <div className="flex justify-around my-3">
+        {/* display left if it's not the first one */}
+        {offset !== 0 && (
+          <div className="border rounded-full items-center justify-center flex border-orange text-orange">
+            <Icon
+              name="keyboard_arrow_left"
+              onClick={() => changePageHandler("left")}
+              size="10"
+              className=""
+            />
+          </div>
+        )}
+        <div className="border rounded-full items-center justify-center flex border-orange text-orange">
+          <Icon
+            name="keyboard_arrow_right"
+            onClick={() => changePageHandler("right")}
+            size="10"
+          />
+        </div>
+      </div>
+
       {/* pokemons card */}
       <div className="grid grid-cols-2 gap-4">
         {pokemons.map((pokemon, idx) => {
